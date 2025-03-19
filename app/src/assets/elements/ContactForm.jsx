fa4edabcfc98bfd68796dbc272bf6841d6917b85
DaneVN <Dane.vN23@gmail.com>
2025-03-19 14:12:15 +0200

    Formspree import replaced with raw html

--- app/src/assets/elements/ContactForm.jsx
+++ app/src/assets/elements/ContactForm.jsx
@@ -1,41 +1,28 @@
 import React from "react";
-import { useForm, ValidationError } from "@formspree/react";
 
 export default function ContactForm() {
-  const [state, handleSubmit] = useForm("meoaeeda");
-  if (state.succeeded) {
-    return <p>Thanks for your message!</p>;
-  }
   return (
     <form
-      onSubmit={handleSubmit}
+      action="https://formspree.io/f/meoaeeda"
+      method="POST"
       className="grow flex flex-col gap-5 items-start mx-5"
     >
       <div>
         <label htmlFor="email">Email Address</label>
         <input
           className="ml-1 rounded-md border-[var(--clf-dark)] border-2"
-          id="email"
           type="email"
           name="email"
         />
-        <ValidationError prefix="Email" field="email" errors={state.errors} />
       </div>
       <div className="grid grid-cols-3 gap-4 w-full">
         <textarea
           className="ml-1 rounded-md col-span-2 col-start-1 border-[var(--clf-dark)] border-2"
-          id="message"
           name="message"
-        />
-        <ValidationError
-          prefix="Message"
-          field="message"
-          errors={state.errors}
         />
         <button
           className="bg-[var(--clb-accent)] rounded-md col-start-3"
           type="submit"
-          disabled={state.submitting}
         >
           Submit
         </button>
