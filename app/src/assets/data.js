ef9e31cc1df3be8979f2486b085770096987c16c
DaneVN <Dane.vN23@gmail.com>
2025-05-02 10:55:29 +0200

    HLJ Project Added

--- app/src/assets/data.js
+++ app/src/assets/data.js
@@ -1,14 +1,23 @@
 const frontEndProjects = [
   {
     index: 0,
-    name: "Kanban App",
-    desc: `A simple laptop-only todo website that relies on local storage and a data file to persist tasks. Makes use of Local Storage, 
-    JS/HTML/CSS and Modal windows to achieve a suitable UI design`,
-    link: "https://dane-kanban.netlify.app",
-    imgPath: "/assets/dane-kanban.png",
+    name: "Homeloan Junction Landing page",
+    desc: `A mobile-first, laptop friendly webpage that helps clients of the Real estate Financing company, Homeloan Junction
+    find what information and to whom they need to send that information to. It makes use of the React-router-DOM package and 
+    TailwindCSS For styling`,
+    link: "https://homeloan-junction-commercial.netlify.app/",
+    imgPath: "/assets/HLJ-Landingpage.png",
   },
   {
     index: 1,
+    name: "RE/MAX Tracking app",
+    desc: `A mobile-based webpage that helps agents keep track of all their monthly activities, and provide stats on the data 
+    collected in the current and previous month`,
+    link: "https://remax-tracking-app.netlify.app/",
+    imgPath: "/assets/REMAXTrackingApp.png",
+  },
+  {
+    index: 2,
     name: "Dashboard extention",
     desc: `A custom dashboard extention that uses API's and JavaScript to give live data on weather, Dogecoin and the current time. 
     The background changes everytime the page loads`,
@@ -16,7 +25,15 @@ const frontEndProjects = [
     imgPath: "/assets/dashboard-extention.png",
   },
   {
-    index: 2,
+    index: 3,
+    name: "Kanban App",
+    desc: `A simple laptop-only todo website that relies on local storage and a data file to persist tasks. Makes use of Local Storage, 
+    JS/HTML/CSS and Modal windows to achieve a suitable UI design`,
+    link: "https://dane-kanban.netlify.app",
+    imgPath: "/assets/dane-kanban.png",
+  },
+  {
+    index: 4,
     name: "Fictional bank Landing page",
     desc: `A bank Landing page for a fictional Bank catering for software developers specifically. It makes use of HTML/CSS 
     and JavaScript`,
@@ -24,20 +41,12 @@ const frontEndProjects = [
     imgPath: "/assets/dane-bank-landingpage.png",
   },
   {
-    index: 3,
+    index: 5,
     name: "Podcast app",
     desc: `A website that functions the same as a generic podcast app using API's State Store and other higher level 
     programming concepts`,
     link: "https://dane-podcast-app.netlify.app/",
     imgPath: "/assets/dane-podcast-app.png",
-  },
-  {
-    index: 4,
-    name: "RE/MAX Tracking app",
-    desc: `A mobile-based webpage that helps agents keep track of all their monthly activities, and provide stats on the data 
-    collected in the current and previous month`,
-    link: "https://remax-tracking-app.netlify.app/",
-    imgPath: "/assets/REMAXTrackingApp.png",
   },
 ];
 
